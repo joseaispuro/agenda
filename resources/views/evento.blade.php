f77b1a36d9bb84930799e52846748343515095a0
Jose Aispuro <joseaispuro2481@gmail.com>
2021-09-01 18:54:15 +0000

    Se elimina lo de app repetida en las demas vistas

--- resources/views/evento.blade.php
+++ resources/views/evento.blade.php
@@ -12,24 +12,7 @@
 @endpush
 
 @section('content')
-<div id="app">
-    
-<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#"><i class="fas fa-calendar-week"></i> Agenda Municipal</a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <img src="http://servicios.mazatlan.gob.mx/predial/img/logo-white.png" class="logo" alt="">
-  <!--<input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">-->
-  <ul class="navbar-nav px-3">
-    <li class="nav-item text-nowrap">
-      <a class="nav-link" href="{{url('/logout')}}">Cerrar Sesión</a>
-    </li>
-  </ul>
-</header>
-
-<div class="container-fluid">
   <div class="row">
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
       <div class="position-sticky pt-3">
@@ -213,12 +196,5 @@
           
     </main>
   </div>
-</div>
 
-
-</div>
-
-    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
-
-  
 @endsection
