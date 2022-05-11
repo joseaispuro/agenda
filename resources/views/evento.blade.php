ab3ef9f2dea9e73af0c23ed3e4b2fb39526ee2ab
Jose Aispuro <joseaispuro2481@gmail.com>
2022-05-11 19:11:29 +0000

    quite el debug

--- resources/views/evento.blade.php
+++ resources/views/evento.blade.php
@@ -12,7 +12,6 @@
 @endpush
 
 @section('content')
-@{{$data}}
   <div class="row">
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
       <div class="position-sticky pt-3">
